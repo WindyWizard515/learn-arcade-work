94683d6bb52a73c0ead7c16b47afb0a6772c6201
Paul Hudson <paul@paul.localdomain>
2024-06-19 13:11:05 -0500

    Working on Weapons, Tomorrow I will do intereactables and also set up the enemys and how to lock the player in the room while fighting an enemy.

--- Lab 06 - Text Adventure/Text_Adventure.py
+++ Lab 06 - Text Adventure/Text_Adventure.py
@@ -1,1 +1,92 @@
-import arcade
+""" Create All of the Classes """
+
+# Create a Class called Room()
+class Room():
+    def __init__(self, description, north, east, south, west):
+        self.description = description
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+class Player():
+
+    def __init__(self, health, armor, weapon):
+        self.health = health
+        self.armor = armor
+        self.weapon = weapon
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Create a Weapon Class
+class Weapon():
+        """ Create a Weapon Class """
+        def __init__(self, name, damage):
+            self.name = name
+            self.damage = damage
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Create an Enemy Class
+class Enemy():
+    """ Create an Enemy Class """
+    def __init__(self, name, damage, health):
+        self.name = name
+        self.damage = damage
+        self.health = health
+
+
+
+""" Create all of the Instances of the Classes that we made above """
+
+"""                          Rooms                              """
+# Create the Armory Room
+armory = Room("\n\n       You are in the Armory, you see a big wall of weapons.\
+                \n              You may take a weapon or move East or West\n\n", None, 1, None, 2)
+
+
+# Create the Kitchen Room
+kitchen = Room("\n\n       You are in the Kitchen, you see a box of bread that is just in your reach.\
+                \n                   You may pick up the box of bread or move West\n\n", None, None, None, 0)
+
+
+# Create the Library Room
+library = Room("\n\n       You are in the Library, you see nothing of interest.\
+                \n                      You may move North or East\n\n", 3, 0, None, None)
+
+
+# Create the Child's Bedroom Room
+child_bedroom = Room("\n\n       You are in the Child's Bedroom, you see nothing of interest.\
+                        \n                         You may move East or South\n\n", None, 4, 2, None)
+
+
+# Create the Bedroom Room
+bedroom = Room("\n\n       You are in the Bedroom, along a dusty bookshelf, you can see a book the stands out more than the rest.. There is less dust on it!\
+                \n                                                 You may interact with The Book or move West\n\n", None, None, None, 3)
+
+
+boss_room = Room("\n\n      You are now in the Boss Room! The passageway Closed behind you and you can see a weak and frail man sitting in the center of the arena,\
+                        suddenly, he starts to glow with an eerie light, and starts rising in the air! You can't see anything, but suddenly it seems like time freezes.\
+                        Then in the middle of the arena you see the Old Man, but suddenly he points what looks like a wand at you and suddenly a big bolt of lightning crashes out of the want at you\
+                                                                        He missed... This time.\n\n", None, None, None, None)
+
+
+"""                                 Weapons                     """
+# War Ax deals 20 damage
+war_ax = Weapon("War Axe", 20)
+
+# Spear deals 15 damage
+spear = Weapon("Spear", 15)
+
+# Katana deals 15 damage
+katana = Weapon("Katana", 15)
+
+# Shortsword deals 15 damage
+shortsword = Weapon("Shortsword", 10)
+
+# Thief's Dagger
+theif_dagger = Weapon("Theif's Dagger", 10)
+
+# Fists
+barehanded = Weapon("Barehanded", 5)
--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,1 +1,124 @@
 from Text_Adventure import *
+
+def main():
+
+    # Create an empty list where we will append all of the rooms that we create
+    room_list = []
+
+    # Append all of the variables to Room_ list
+
+    # Armory
+    room_list.append(armory)
+
+    # Kitchen
+    room_list.append(kitchen)
+
+    # Library
+    room_list.append(library)
+
+    # Child's Bedroom
+    room_list.append(child_bedroom)
+
+    # Bedroom
+    room_list.append(bedroom)
+
+    # Boss Room
+    room_list.append(boss_room)
+
+    """ Create a variable that we will change based on the user input for each room. The room numbers are:
+                    0=Armory 1=Kitchen 2=Library 3=Child's Bedroom 4=Bedroom 5=Boss Room"""
+    current_room = 0
+
+    # Create an instance of the Weapon class whose paramaters are name, and damage
+    weapon_list = []
+
+    # Append all of the weapons to weapon_list
+    # Append the War ax
+    weapon_list.append(war_ax)
+
+    # Append the Spear
+    weapon_list.append(spear)
+
+    # Append the Katana
+    weapon_list.append(katana)
+
+    # Append the Shortsword
+    weapon_list.append(shortsword)
+
+    # Append the Thief's Dagger
+    weapon_list.append(theif_dagger)
+
+    # Append the Fists
+    weapon_list.append(barehanded)
+
+    """ Will Change based on the user input in the armory, the weapn indexes are:
+                    0=War Axe 1=Spear 2=Katana 3=Shortsword 4=Theif's Dagger 5=Fists"""
+    chosen_weapon = weapon_list[5]
+
+    # Create an instance of the Player class for health, armor, weapon, and the amount of damage they do
+    
+    player_status = Player(100, 5, chosen_weapon)
+
+
+
+
+
+
+    
+    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    """ Create the main loop of the game that includes the input of the player """
+    while True:
+
+        # Print the description of the room you are currently in
+        print(room_list[current_room].description)
+
+        # Ask the user what they would like to do
+        user_input = input("                   What do you choose to do?  ")
+
+        # Testing for all of the commands that the user could input:
+
+        # North
+        if user_input.lower() == "n" or user_input.lower() == "north":
+            next_room = room_list[current_room].north
+            if next_room == None:
+                print("\n                         You can't go that way!")
+            else:
+                current_room = next_room
+
+        # East
+        elif user_input.lower() == "e" or user_input.lower() == "east":
+            next_room = room_list[current_room].east
+            if next_room == None:
+                print("\n                         You can't go that way!")
+            else:
+                current_room = next_room
+
+        # South
+        elif user_input.lower() == "s" or user_input.lower() == "south":
+            next_room = room_list[current_room].south
+            if next_room == None:
+                print("\n                         You can't go that way!")
+            else:
+                current_room = next_room
+
+        # West
+        elif user_input.lower() == "w" or user_input.lower() == "west":
+            next_room = room_list[current_room].west
+            if next_room == None:
+                print("\n                         You can't go that way!")
+            else:
+                current_room = next_room
+
+        # Interactables
+
+        # Quit
+        elif user_input.lower() == "q" or user_input.lower() == "quit":
+            print("\n                    You have now quit the game :(\n")
+            break
+
+        # User input is not undersood
+        else:
+            print(f"\nThe user input: '{user_input}' was not understood.\n              Please Try again.")
+
+
+main()
