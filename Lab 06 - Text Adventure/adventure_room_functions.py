5fcac434dedc55b4c71a346e848e5de2cdc7b11d
Paul Hudson <paul@paul.localdomain>
2024-06-23 15:35:13 -0500

    Condensing everything into one file, will keep the other files but do not touch them before finishing the arcade projects

--- Lab 06 - Text Adventure/adventure_room_functions.py
+++ Lab 06 - Text Adventure/adventure_room_functions.py
@@ -1,8 +1,10 @@
+from adventure_weapons import *
 """                              Make the definitions for all the interactables in the rooms                            """
+
+""" The function for the armory room """
 def wall_of_weapons():
-    global weapon_choice
+    from lab_06 import weapon_choice, weapon_list
     global chosen_weapon
-    global weapon_list
     print("\nChoose a Weapon to aid you on your journey\n")
     print("1: War Axe, Damage = 25 speed = 1")
     print("2: Spear, Damage = 15, speed = 1.5")
@@ -11,14 +13,32 @@ def wall_of_weapons():
     print("5: Theif's Dagger, damage 10, speed 2.5")
     print("6: Fists, damage 5, speed 1")
     weapon_choice = input("\nEnter the number of what weapon you would like: ")
-    if int(weapon_choice) > 6 or int(weapon_choice) < 1:
+    if weapon_choice.lower() == 'n' or weapon_choice.lower() == 'no':
+        print("Ok")
+    elif int(weapon_choice) > 6 or int(weapon_choice) < 1:
         print("You can't pick that number!")
-    else:
+    elif weapon_choice.isdigit() == True:
         chosen_weapon = weapon_list[int(weapon_choice)]
         print(f"The weapon you chose was {chosen_weapon.name}!")
+    else:
+        print(f"User input {weapon_choice} not understood")
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
+""" The function for the kitchen room """
+def ration_of_food():
+    from lab_06 import add_inventory
+    inventory_answer = input("Do you want to add a Ration of food to your inventory? ")
+    if inventory_answer.lower == "y" or inventory_answer.lower() == "yes":
+        print("You now have a ration of food in your inventory")
+        add_inventory("Ration of food")
+    elif inventory_answer.lower == "n" or inventory_answer.lower == "no":
+        print("Ok")
+    else:
+        print(f"User input {inventory_answer} not understood.")
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 """ The instructions of how to play the game and move around """
 
 def instructions_print():
@@ -36,4 +56,4 @@ def instructions_print():
     print("If you type in 'weapon' it will display current weapon {Work In Progress}")
     print("If you want to quit the game just press 'q' or type 'quit'")
     print("Follow the prompts and descriptions provided in the game to explore, solve puzzles,\
-and advance through the story.\n")
+ and advance through the story.\n")
--- Lab 06 - Text Adventure/everything.py
+++ Lab 06 - Text Adventure/everything.py
@@ -1,8 +1,165 @@
-# from adventure_classes import *
-# from adventure_weapons import *
-from adventure_room_functions import *
 import os
+"""                                     Create All of the Classes                              """
 
+# Create a Class called Room()
+class Room:
+    def __init__(self, description, north, east, south, west, interactables, interact_action):
+        self.description = description
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+        self.interactables = interactables
+        self.interact_action = interact_action
+
+    def interact(self):
+        """
+        Property for interactables that runs a piece of code when accessed.
+        """
+        self.interact_action()
+        
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+class Player():
+
+    def __init__(self, health, armor, weapon):
+        self.health = health
+        self.armor = armor
+        self.weapon = weapon
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Create a Weapon Class
+class Weapon():
+        """ Create a Weapon Class """
+        def __init__(self, name, damage):
+            self.name = name
+            self.damage = damage
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Create an Enemy Class
+class Enemy():
+    """ Create an Enemy Class """
+    def __init__(self, name, damage, health):
+        self.name = name
+        self.damage = damage
+        self.health = health
+
+
+"""                                 Weapons                     """
+# Create an instance of the Weapon class whose paramaters are name, and damage
+# War Ax deals 20 damage
+war_ax = Weapon("War Axe", 20)
+
+# Spear deals 15 damage
+spear = Weapon("Spear", 15)
+
+# Katana deals 15 damage
+katana = Weapon("Katana", 15)
+
+# Shortsword deals 15 damage
+shortsword = Weapon("Shortsword", 10)
+
+# Thief's Dagger
+theif_dagger = Weapon("Theif's Dagger", 10)
+
+# Fists
+barehanded = Weapon("Barehanded", 5)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+# Making the variable for the list of weapons that I am going to append all of the weapons to
+weapon_list = []
+
+# Append a placeholder so I dont have to find the difference of what the player puts in on the wall of weapons screen
+weapon_list.append("placeholder")
+
+# Append all of the weapons to weapon_list
+# Append the War ax
+weapon_list.append(war_ax)
+
+# Append the Spear
+weapon_list.append(spear)
+
+# Append the Katana
+weapon_list.append(katana)
+
+# Append the Shortsword
+weapon_list.append(shortsword)
+
+# Append the Thief's Dagger
+weapon_list.append(theif_dagger)
+
+# Append the Fists
+weapon_list.append(barehanded)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+""" Will Change based on the user input in the armory, the weapon indexes are:
+                1=War Axe 2=Spear 3=Katana 4=Shortsword 5=Theif's Dagger 6=Fists The default weapons are Fists"""
+
+weapon_choice = 6
+chosen_weapon = weapon_list[weapon_choice]
+
+"""                              Make the definitions for all the interactables in the rooms                            """
+
+""" The function for the armory room """
+def wall_of_weapons():
+    # from lab_06 import weapon_choice, weapon_list, chosen_weapon
+    global chosen_weapon
+    print("\nChoose a Weapon to aid you on your journey\n")
+    print("1: War Axe, Damage = 25 speed = 1")
+    print("2: Spear, Damage = 15, speed = 1.5")
+    print("3: Katana, Damage = 15, speed = 1, block = 5")
+    print("4: Shortsword, damage 10, speed 2")
+    print("5: Theif's Dagger, damage 10, speed 2.5")
+    print("6: Fists, damage 5, speed 1")
+    weapon_choice = input("\nEnter the number of what weapon you would like: ")
+    if weapon_choice.lower() == 'n' or weapon_choice.lower() == 'no':
+        print("Ok")
+    elif int(weapon_choice) > 6 or int(weapon_choice) < 1:
+        print("You can't pick that number!")
+    elif weapon_choice.isdigit() == True:
+        chosen_weapon = weapon_list[int(weapon_choice)]
+        print(f"The weapon you chose was {chosen_weapon.name}!")
+    else:
+        print(f"User input {weapon_choice} not understood")
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+""" The function for the kitchen room """
+def ration_of_food():
+    # from lab_06 import add_inventory
+    inventory_answer = input("Do you want to add a Ration of food to your inventory? ")
+    if inventory_answer.lower == "y" or inventory_answer.lower() == "yes":
+        print("You now have a ration of food in your inventory")
+        add_inventory("Ration of food")
+    elif inventory_answer.lower == "n" or inventory_answer.lower == "no":
+        print("Ok")
+    else:
+        print(f"User input {inventory_answer} not understood.")
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""" The instructions of how to play the game and move around """
+
+def instructions_print():
+    """ Print the directions and how to control the player """
+    print("\n\nWelcome to the text adventure game \"The Dark Tower.\"")
+    print("To play, use directional commands to navigate:")
+    print("- Type 'n' or 'north' to go north,")
+    print("- Type 'e' or 'east' to go east,")
+    print("- Type 's' or 'south' to go south,")
+    print("- Type 'w' or 'west' to go west.\n")
+    print("To interact with objects or characters in your surroundings, press 'i', or type 'interact'.")
+    # print("Access your inventory by pressing 'b' or typing 'backpack' or 'inventory'.")
+    # print("Use 'u' or 'use' to utilize any items you have collected.")
+    # print("If you encounter an enemy, press 'a' or type 'attack' to engage in combat.")
+    print("If you type in 'weapon' it will display current weapon {Work In Progress}")
+    print("If you want to quit the game just press 'q' or type 'quit'")
+    print("Follow the prompts and descriptions provided in the game to explore, solve puzzles,\
+ and advance through the story.\n")
 """            Create all of the Instances of the Classes that we made in adventure_classes """
 
 """                                                Rooms                                       """
