aedb4f55a2d62db55d0405f8a444380ad210e8c0
Paul Hudson <paul@paul.localdomain>
2024-06-22 19:11:11 -0500

    Sorting Files

---
+++ Lab 06 - Text Adventure/adventure_room_functions.py
@@ -0,0 +1,39 @@
+"""                              Make the definitions for all the interactables in the rooms                            """
+def wall_of_weapons():
+    global weapon_choice
+    global chosen_weapon
+    global weapon_list
+    print("\nChoose a Weapon to aid you on your journey\n")
+    print("1: War Axe, Damage = 25 speed = 1")
+    print("2: Spear, Damage = 15, speed = 1.5")
+    print("3: Katana, Damage = 15, speed = 1, block = 5")
+    print("4: Shortsword, damage 10, speed 2")
+    print("5: Theif's Dagger, damage 10, speed 2.5")
+    print("6: Fists, damage 5, speed 1")
+    weapon_choice = input("\nEnter the number of what weapon you would like: ")
+    if int(weapon_choice) > 6 or int(weapon_choice) < 1:
+        print("You can't pick that number!")
+    else:
+        chosen_weapon = weapon_list[int(weapon_choice)]
+        print(f"The weapon you chose was {chosen_weapon.name}!")
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+""" The instructions of how to play the game and move around """
+
+def instructions_print():
+    """ Print the directions and how to control the player """
+    print("\n\nWelcome to the text adventure game \"The Dark Tower.\"")
+    print("To play, use directional commands to navigate:")
+    print("- Type 'n' or 'north' to go north,")
+    print("- Type 'e' or 'east' to go east,")
+    print("- Type 's' or 'south' to go south,")
+    print("- Type 'w' or 'west' to go west.\n")
+    print("To interact with objects or characters in your surroundings, press 'i', or type 'interact'.")
+    # print("Access your inventory by pressing 'b' or typing 'backpack' or 'inventory'.")
+    # print("Use 'u' or 'use' to utilize any items you have collected.")
+    # print("If you encounter an enemy, press 'a' or type 'attack' to engage in combat.")
+    print("If you type in 'weapon' it will display current weapon {Work In Progress}")
+    print("If you want to quit the game just press 'q' or type 'quit'")
+    print("Follow the prompts and descriptions provided in the game to explore, solve puzzles,\
+and advance through the story.\n")
